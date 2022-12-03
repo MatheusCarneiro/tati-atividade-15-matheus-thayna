4a1f9ecd1ee5d3521633ec12a904f207c9960a1e
MatheusCarneiro <matheuscarneiro106@gmail.com>
2022-12-02 22:56:25 -0300

    Detalhes do livro

--- atividade_15/lib/pages/detalhes_livro.dart
+++ atividade_15/lib/pages/detalhes_livro.dart
@@ -21,15 +21,71 @@ class DetalhesLivro extends StatelessWidget {
     final livro = repositorio.livros.firstWhere((element) => element.id == id);
 
     return Scaffold(
-      appBar: AppBar(title: Text("Detalhes do livro")),
-      body: Container(
-        child: Column(
-          children: [
-            Image.network(livro.urlCapa),
-            Text(livro.titulo),
-          ],
-        ),
-      ),
-    );
+        appBar: AppBar(title: Text("Detalhes do livro")),
+        body: Container(
+          child: Row(children: [
+            Container(
+                width: 300,
+                height: 450,
+                padding: EdgeInsets.fromLTRB(20, 40, 10, 0),
+                child: Column(
+                  children: [
+                    Image.network(livro.urlCapa),
+                  ],
+                )),
+            Container(
+              padding: EdgeInsets.fromLTRB(10, 40, 20, 0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    livro.titulo,
+                    style: TextStyle(fontSize: 26),
+                  ),
+                  SizedBox(height: 100),
+                  Row(
+                    children: [
+                      Icon(Icons.create_rounded),
+                      Text(
+                        livro.autor,
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Icon(Icons.collections_bookmark_rounded),
+                      Text(
+                        livro.edicao,
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Icon(Icons.calendar_month_rounded),
+                      Text(
+                        livro.dataLeitura,
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Icon(Icons.description),
+                      Text(
+                        livro.opiniao,
+                        style: TextStyle(fontSize: 14),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ]),
+        ));
   }
 }
